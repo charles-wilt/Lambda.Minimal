2914c2933f0a7380f7c22b27f06fd9e8389b11e0
Charles.Wilt@coxautoinc.com <Charles.Wilt@coxautoinc.com>
2024-02-26 15:28:00 -0700

    Add project files.

---
+++ Lambda.Minimal/src/Lambda.Minimal/Config/PolicyConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Lambda.Minimal.Config
+{
+    public class PolicyConstants
+    {
+        public const string DealerOsAccountingRead = "DealerOsAccountingRead";
+        public const string DealerOsAccountingWrite = "DealerOsAccountingWrite";
+
+        public const string DealerOsAccountingReadScope = "dtdms.dealeros.accounting.read";
+        public const string DealerOsAccountingWriteScope = "dtdms.dealeros.accounting.write";
+    }
+}
---
+++ Lambda.Minimal/src/Lambda.Minimal/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.DataProtection;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
+// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
+builder.Services.AddHttpClient();
+
+// builder.Services.AddAuthorization();
+
+
+builder.Services.AddDataProtection().UseEphemeralDataProtectionProvider();
+
+//builder.Services.AddAuthentication();
+
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapGet("/", () => "Welcome to running ASP.NET Core Minimal API on AWS Lambda");
+
+app.Run();
